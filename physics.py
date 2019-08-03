10c3a73414164a6d7971d01f07a648ddfaf8055e
halucka <halucka@gmail.com>
2019-08-03 15:11:36 +0200

    fixes

--- physics.py
+++ physics.py
@@ -1,23 +1,18 @@
 x_position = 0
-y_position = 0
+y_position = 10
 x_velocity = 10
 y_velocity = 8
 min_y_position = 0
 
-def Floor(x):
-    rounded = int(x)
-    if rounded > x:
-        rounded -= 1
-    return rounded
+g = 9.81
 
-def Gravity(y_velocity, y_position, Floor, min_y_position):
+def gravity(y_velocity, y_position, min_y_position):
     if y_position > min_y_position:
-        g = Floor(y_velocity/2)
         y_velocity -= g
     return y_velocity
 
-while True:
-    y_velocity = Gravity(y_velocity, y_position, Floor, min_y_position)
+while y_position > min_y_position:
+    y_velocity = gravity(y_velocity, y_position, min_y_position)
     x_position += x_velocity
     y_position += y_velocity
-    print(y_velocity)
+    print(x_position, y_position, x_velocity, y_velocity)
