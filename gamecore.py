93fff44d164685ef648ef48470364602fc62692a
Jakub Komenda <jakub@192.168.1.14>
2019-08-04 11:09:43 +0200

    Hey, added an empty game loop and a screen init

--- gamecore.py
+++ gamecore.py
@@ -2,3 +2,11 @@ import sys
 import pygame
 
 pygame.init()
+
+infoObject = pygame.display.Info()
+screen = pygame.display.set_mode((infoObject.current_w, infoObject.current_h))
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
