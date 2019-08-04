0fae311301c47bb1b5dd7c1524025e38f922f536
Jakub Komenda <jakub@192.168.1.14>
2019-08-04 13:48:55 +0200

    Removed unneceseary code and added timestep

--- physics.py
+++ physics.py
@@ -5,14 +5,14 @@ y_velocity = 8
 min_y_position = 0
 
 g = 9.81
+time = 0.001
 
 def gravity(y_velocity, y_position, min_y_position):
-    if y_position > min_y_position:
-        y_velocity -= g
+    y_velocity -= g * time
     return y_velocity
 
 while y_position > min_y_position:
     y_velocity = gravity(y_velocity, y_position, min_y_position)
-    x_position += x_velocity
-    y_position += y_velocity
+    x_position += x_velocity * time
+    y_position += y_velocity 
     print(x_position, y_position, x_velocity, y_velocity)
