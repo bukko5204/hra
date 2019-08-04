50b3e371b518857650af9d1a98b8fb6203fefb1c
Jakub Komenda <jakub@192.168.1.14>
2019-08-04 11:33:53 +0200

    Added enemies class0_1

--- enemies.py
+++ enemies.py
@@ -1,1 +1,18 @@
+import pygame
 
+class enemy(self,x, y, y_velocity, x_velocity, img):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.img = img
+
+    def movement(self, y_velocity, x_velocity):
+        self.x_velocity = x_velocity
+        self.y_velocity = y_velocity
+
+    def position(self.x, self.y):
+        position = [self.x, self.y]
+
+    def draw(position, self.img):
+        pygame.draw(position, self.imgimg)
