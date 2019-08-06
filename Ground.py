8705229a72c1ce9665a5ea592d76caec4d28a346
Jakub Komenda <jakub@192.168.1.14>
2019-08-06 13:21:18 +0200

    Debugged the enemies module and added new funcionality

--- Ground.py
+++ Ground.py
@@ -1,1 +1,12 @@
+class Ground(self, y_position, lenght, img):
 
+    def __init__(self, y_position, lenght):
+        self.y_position = y_position
+        min_y_position = self.y_position
+        self.lenght = lenght
+        self.img = img
+
+    def draw(self.y_position, self.img):
+        pygame.draw(self.y_position, self.img)
+
+    
--- enemies.py
+++ enemies.py
@@ -1,20 +1,40 @@
+import os
 import pygame
 
-class enemy(self,x, y, y_velocity, x_velocity, img):
+_image_library = {}
 
-    def __init__(self, x, y):
+def get_image(path):
+        global _image_library
+        img = _image_library.get(path)
+        if img == None:
+                canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+                img = pygame.image.load(canonicalized_path)
+                _image_library[path] = image
+        return imag
+
+class enemy:
+
+    def __init__(self, x, y, x_velocity, y_velocity, img):
         self.x = x
         self.y = y
+        self.y_velocity = y_velocity
+        self.x_velocity = x_velocity
         self.img = img
 
-    def movement(self, y_velocity, x_velocity):
-        self.x_velocity = x_velocity
-        self.y_velocity = y_velocity
-
-    def position(self.x, self.y):
+    def position(self):
         position = [self.x, self.y]
 
-    def draw(position, self.img):
+    def draw(self):
         pygame.draw(position, self.img)
 
-spikes = enemy(spikes, 10, 0, 0, 0, autodraw 04_08_2019.png)
+class spikes(enemy):
+    x = 10
+    y = 10
+    y_velocity = 0
+    x_velocity = 0
+    img = get_image()
+    img = pygame.image.load('autodraw 04_08_2019.png')
+
+
+spikes = spikes()
+#toxic_rain = enemy(toxic_rain, 20, 15, 0, 0, 2.5, #img)
