c052d323cdb6b98c74900903b3b12333381f3540
Jakub Komenda <jakub@192.168.1.12>
2019-08-06 19:59:33 +0200

    Added some functionality

---
+++ Ground.py
@@ -0,0 +1,11 @@
+class Ground():
+
+    def __init__(self, y_position, lenght, rotation):
+        self.y_position = y_position
+        min_y_position = self.y_position
+        self.lenght = lenght
+        self.img = img
+        seslf.rotation = rotation
+
+    def draw(self):
+        pygame.draw()
