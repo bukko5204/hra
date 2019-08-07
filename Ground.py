adf030746f6673fbde15e4411179252e16a6c526
Jakub Komenda <teodor.komenda@gmail.com>
2019-08-07 20:25:45 +0200

    Update Ground.py
    Typo

--- Ground.py
+++ Ground.py
@@ -5,7 +5,7 @@ class Ground():
         min_y_position = self.y_position
         self.lenght = lenght
         self.img = img
-        seslf.rotation = rotation
+        self.rotation = rotation
 
     def draw(self):
         pygame.draw()
