16e78e7210f7b0a822ec3381c82a780ffac3a910
Jakub Komenda <teodor.komenda@gmail.com>
2019-08-12 14:10:50 +0200

    There is still a bug that needs a look

--- Ground.py
+++ Ground.py
@@ -6,20 +6,22 @@ infoObject = pygame.display.Info()
 width, height = infoObject.current_w, infoObject.current_h
 screen = pygame.display.set_mode((width, height), 0, 32)
 
-class ground():
+class ground:
 
     def __init__(self, y, lenght, rotation):
         self.y = y
         min_y = self.y
         self.lenght = lenght
-        self.img = img
+        #self.img = img
         self.rotation = rotation
 
     def draw(self):
-        self.position(self.y, height*0.95)
+        self.position = (self.y , height*0.95)
+        ground = pygame.draw.rect(screen, (255, 255, 255), (25, 35, 100, 100))
+        screen.blit(ground, self.position)
 
 
-earth = ground()
+earth = ground(10, 10, 10)
 
 black = 0, 0, 0
 
@@ -30,5 +32,5 @@ while 1:
             sys.exit();
 
     screen.fill(black)
-    ground.draw()
+    earth.draw()
     pygame.display.flip()
